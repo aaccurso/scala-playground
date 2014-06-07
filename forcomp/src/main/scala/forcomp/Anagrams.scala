7877a3c5c0bc6aedbeb9a597f2dbc090512cda9f
aaccurso <accurso.alan@gmail.com>
2014-06-06 23:33:09 -0300

    Implements wordAnagrams

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -57,10 +57,15 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] =
+    loadDictionary.groupBy(wordOccurrences(_))
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] =
+    dictionaryByOccurrences.get(wordOccurrences(word)) match {
+    case Some(words: List[Word]) => words
+    case _ => List()
+  }
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
--- forcomp/src/test/scala/forcomp/maps.sc
+++ forcomp/src/test/scala/forcomp/maps.sc
@@ -10,4 +10,6 @@ object maps {
   (for(c <- char_map; o <- c._2) yield (c._1, o._2)).toList.sorted
                                                   //> res0: List[(Char, Int)] = List((a,2), (c,1), (d,3), (l,1), (s,1))
 	List("a", "b").foldRight("")(_+_)         //> res1: String = ab
+	char_map.get('a')                         //> res2: Option[List[(Char, Int)]] = Some(List((a,3), (a,2)))
+	char_map.get('z')                         //> res3: Option[List[(Char, Int)]] = None
 }
